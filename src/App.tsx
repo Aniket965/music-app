953fa47de858b500441a62bf655a0ed97cadedf2
aniket965 <aniket965.as@gmail.com>
2020-08-29 13:32:22 +0530

    feat: tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 
 import './App.css';
-
+import './styles.css';
 import { ApolloProvider } from '@apollo/client';
 import { useQuery } from '@apollo/client';
 import ArtistCard from './components/ArtistCard';
 import client from './apolloclient';
-import {queries} from './queries';
+import { queries } from './queries';
 
 
 function ArtistsList({ query }) {
@@ -24,12 +24,18 @@ function App() {
   const [artistquery, setArtistQuery] = useState('');
 
   return (
-    <div className="App">
+    <div className="App flex justify-center">
       <ApolloProvider client={client}>
-        <h1> Search Artists by Name </h1>
-        <input type="text" placeholder="Search Artists..." onChange={e => setArtistQuery(e.target.value)} />
-        <div style={{ marginTop: '2rem' }}>
+        <div className="container">
+        <div>
+          <h1 className="text-6xl text-purple-600"> Search Artists by Name </h1>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text" placeholder="Search Artists..." onChange={e => setArtistQuery(e.target.value)} />
+        </div>
+        <div style={{ marginTop: '2rem' }}  >
           {artistquery.length < 3 ? (<div> Looking for artists...</div>) : <ArtistsList query={artistquery} />}
+        </div>
         </div>
       </ApolloProvider>
     </div>
