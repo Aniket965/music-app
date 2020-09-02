ef8500ed31ec7aec6b7e09da8fefca491ee50a7c
aniket965 <aniket965.as@gmail.com>
2020-09-02 23:55:16 +0530

    feat:debouncing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles.css';
 import { useQuery } from '@apollo/client';
 import ArtistCard from '../components/ArtistCard';
 import { queries } from '../queries';
+import { useDebounce } from '../utilites/debounce';
+
+
 
 
 function ArtistsList({ query }) {
+    const SEARCH_DEBOUNCE_TIMEOUT = 500;
+    const debounceSearch = useDebounce(query, SEARCH_DEBOUNCE_TIMEOUT);
+
     const { loading, error, data } = useQuery(queries.ARTISTS, {
-        variables: { query },
+        variables: { query: debounceSearch },
     });
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error :(</p>;
@@ -24,21 +30,26 @@ function ArtistsList({ query }) {
     </div>
 }
 
+
+
 function Home() {
+
     const [artistquery, setArtistQuery] = useState('Nirvana');
+
+
     return (
         <div className="w-full flex justify-center">
-        <div className="container p-6">
-            <div>
-                <h1 className="text-6xl text-purple-600"> Search Artists by Name </h1>
-                <input
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    type="text" placeholder="Search Artists..." onChange={e => setArtistQuery(e.target.value)} />
+            <div className="container p-6">
+                <div>
+                    <h1 className="text-6xl text-purple-600"> Search Artists by Name </h1>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        type="text" placeholder="Search Artists..." onChange={e => setArtistQuery(e.target.value)} />
+                </div>
+                <div style={{ marginTop: '2rem' }}  >
+                    {artistquery.length < 3 ? (<div> Looking for artists...</div>) : <ArtistsList query={artistquery} />}
+                </div>
             </div>
-            <div style={{ marginTop: '2rem' }}  >
-                {artistquery.length < 3 ? (<div> Looking for artists...</div>) : <ArtistsList query={artistquery} />}
-            </div>
-        </div>
         </div>
     );
 }
